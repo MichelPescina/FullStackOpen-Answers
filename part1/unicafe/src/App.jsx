a11d1280acb0c116b3d62da7ef2f61108641d259
Michel Pescina <gatocorreo27@gmail.com>
2025-04-17 13:34:23 -0600

    Finished exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Statistic = ({text, value}) => {
+  return <p>{text}: {value}</p>
+}
 
+const Button = ({text, clickHandler}) => {
+  return <button onClick={clickHandler}>{text}</button>
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClickHdlr = () => {setGood(good + 1);}
+  const neutralClickHdlr = () => {setNeutral(neutral + 1);}
+  const badClickHdlr = () => {setBad(bad + 1);}
+  
   return (
-    <>
+    <div>
+      <h1>Give feedback</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button text="Good" clickHandler={goodClickHdlr}></Button>
+        <Button text="Neutral" clickHandler={neutralClickHdlr}></Button>
+        <Button text="Bad" clickHandler={badClickHdlr}></Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h2>Statistics</h2>
+      <div>
+        <Statistic text="Good" value={good}></Statistic>
+        <Statistic text="Neutral" value={neutral}></Statistic>
+        <Statistic text="Bad" value={bad}></Statistic>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
