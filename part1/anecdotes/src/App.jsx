32c4454ff60cc361ca90f5d932245460f08cf506
Michel Pescina <gatocorreo27@gmail.com>
2025-04-18 17:45:23 -0600

    Finished exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,18 @@
 import { useState } from 'react'
 
+const Anecdote = ({title, text, votes}) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>
+        {text} 
+        <br></br>
+        has {votes} votes
+      </p>
+    </div>
+  )
+}
+
 const Button = ({text, clickHandler}) => {
   return <button onClick={clickHandler}>{text}</button>
 }
@@ -16,8 +29,10 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
    
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(Math.floor(Math.random() * anecdotes.length))
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const [currTopVal, setCurrTopVal] = useState(0)
+  const [currTopId, setCurrTopId] = useState(0)
 
   const nextClickHdlr = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
@@ -26,15 +41,29 @@ const App = () => {
   const voteClickHdlr = () => {
     let updated = [...votes]
     updated[selected]++
+    if(updated[selected] >= currTopVal) {
+      setCurrTopVal(updated[selected])
+      setCurrTopId(selected)
+    }
     setVotes(updated)
   }
 
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
-      <div>has {votes[selected]} votes</div>
-      <Button text="Vote" clickHandler={voteClickHdlr}></Button>
-      <Button text="Next anecdote" clickHandler={nextClickHdlr}></Button>
+      <Anecdote
+        title="Anecdote of the day"
+        text={anecdotes[selected]}
+        votes={votes[selected]}
+      ></Anecdote>
+      <div>
+        <Button text="Vote" clickHandler={voteClickHdlr}></Button>
+        <Button text="Next anecdote" clickHandler={nextClickHdlr}></Button>
+      </div>
+      <Anecdote
+        title="Anecdote with most votes"
+        text={anecdotes[currTopId]}
+        votes={votes[currTopId]}
+      ></Anecdote>
     </div>
   )
 }
