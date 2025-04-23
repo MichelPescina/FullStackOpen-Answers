5f499a791410f4579d541a0fad8a6e6570e59dbe
Michel Pescina <gatocorreo27@gmail.com>
2025-04-22 18:12:41 -0600

    Finished exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,9 +4,17 @@ const Entry = ({entry}) => {
   return <div>{entry.name} {entry.number}</div>
 }
 
-const Phonebook = ({data}) => {
+const Phonebook = ({data, filter}) => {
+  const hasString = (name, query) => {
+    return name.toLowerCase().includes(query.toLowerCase())
+  }
+
   let elements = data.map((entry) => {
-    return <Entry key={entry.name} entry={entry}></Entry>
+    let elem = null
+    if (filter === '' || hasString(entry.name, filter)) {
+      elem = <Entry key={entry.name} entry={entry}></Entry>
+    }
+    return elem
   })
   return (
     <div>
@@ -17,10 +25,14 @@ const Phonebook = ({data}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "123456789"}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
+  const [filter, setFilter] = useState('')
 
   const factoryUpdate = (setState) => {
     return (event) => setState(event.target.value)
@@ -46,7 +58,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <div>
+          Only show contacts with:
+          <input type='text' onChange={factoryUpdate(setFilter)}/>
+        </div>
+      </div>
       <form onSubmit={handleSubmit}>
+        <h3>Add a new contact</h3>
         <div> 
           Name: <input value={newName} onChange={factoryUpdate(setNewName)}/>
         </div>
@@ -60,8 +79,8 @@ const App = () => {
         </div>
         <div> <button type="submit">Add</button> </div>
       </form>
-      <h2>Numbers</h2>
-      <Phonebook data={persons}></Phonebook>
+      <h3>Numbers</h3>
+      <Phonebook data={persons} filter={filter}></Phonebook>
     </div>
   )
 }
