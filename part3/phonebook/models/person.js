d0ae5ef828a8b73c4bde12697b9d60357080698f
Michel Pescina <gatocorreo27@gmail.com>
2025-06-20 15:52:54 -0600

    Finished exercise 3.20

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -18,7 +18,14 @@ const personSchema = new mongoose.Schema({
         minLength: [3, "Names must be 3 or more characters long!"],
         required: true
     },
-    phone: String
+    phone: {
+        type: String,
+        validate: {
+            validator: (value) => /^\d{2,3}-\d+$/.test(value) && value.length >= 8,
+            message: "{VALUE} is not a valid phone number! Must contain 2 or 3 numbers followed by a hyphen (-) and then more numbers and should have a minimum length of 8."
+        },
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
